8b954b24fe6f5f9384bcdaa256e349131d74decd
Manuel <manuel.kleinschmager@rwth-aachen.de>
2023-06-13 18:17:28 +0200

    Tuesday night update, for working tomorrow. Prepared the function description for mlmc_l.m such that I can implement MLMC methods tomorrow (and MLQMC)

--- Homework 3/functions/mlmc_l.m
+++ Homework 3/functions/mlmc_l.m
@@ -1,5 +1,16 @@
-function [sums, costs] = mlmc_l(l, N0)
+function [sums, costs] = mlmc_l(l, N0, varargin)
 
-CalculateQoI(x,2I,1/(2I),nu)-CalculateQoI(x,I,1/I,nu)
+% inputs:  l = level
+%          N = number of samples
+%          varargin = optional additional user variables
+%
+% output: sums(1) = sum(Y)
+%         sums(2) = sum(Y.^2)
+%         where Y are iid samples with expected value:
+%         E[P_0]           on level 0
+%         E[P_l - P_{l-1}] on level l>0
+%         cost = cost of N samples
+
+% Noah: CalculateQoI(x,2I,1/(2I),nu) - CalculateQoI(x,I,1/I,nu)
 
 end
